f83da98429c2bdae63df857b6649ebb33be04c40
Jack Gammon <jack.gammon@datatonic.com>
2023-05-24 10:06:05 +0100

    feat: strategy interface

---
+++ pipelines/pipelines/algorithm_strategy_interface.py
@@ -0,0 +1,49 @@
+from abc import ABCMeta, abstractmethod
+from typing import List
+
+
+class AlgorithmStrategyInterface(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def train_script_name(self):
+        pass
+
+    @property
+    @abstractmethod
+    def table_suffix(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def hyperparameters(self, **kwargs) -> dict:
+        pass
+
+    @property
+    @abstractmethod
+    def default_model_name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def default_table_suffix(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def script_filename(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def train_container_uri(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def serve_container_uri(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def requirements(self) -> List[str]:
+        pass
